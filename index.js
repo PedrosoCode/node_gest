56dfead724e6eb803bbe2c57470918a3eb51667e
Gabriel <Gabrielpedrofc@gmail.com>
2024-07-15 21:10:30 -0300

    Conserta lista de ativos

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const ambienteRoutes = require('./routes/cadastro_basico/ambiente/ambienteRoutes');
 const authRoutes = require('./routes/auth/authRoutes');
 const parceiroNegocioRoutes = require('./routes/cadastro_basico/parceiro_negocio/parceiroNegocioRoutes');
+const cadBasAtivoRoutes = require('./routes/cadastro_basico/ativo/cadBasAtivoRouter');
 
 const app = express();
 const PORT = 3042;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api', ambienteRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api', parceiroNegocioRoutes);
+app.use('/api', cadBasAtivoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

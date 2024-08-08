655147362034c486711bda449df7dc2c8ee1995e
Gabriel <Gabrielpedrofc@gmail.com>
2024-08-07 23:01:29 -0300

    ajusta para usuário repetido em empresas diferentes

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -3,10 +3,20 @@ const jwt = require('jsonwebtoken');
 const pool = require('../../config/db');
 
 const cadastrarUsuario = async (req, res) => {
-  const { username, email, password, codigo_empresa } = req.body; 
+  const { username, email, password, codigo_empresa } = req.body;
   try {
     if (!password) {
       throw new Error('Password is required');
+    }
+
+    // Verificar se o e-mail já está cadastrado para a mesma empresa
+    const existingUser = await pool.query(
+      'SELECT * FROM tb_cad_usuario WHERE email = $1 AND codigo_empresa = $2',
+      [email, codigo_empresa]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({ message: 'E-mail já cadastrado para esta empresa' });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -27,15 +37,18 @@ const cadastrarUsuario = async (req, res) => {
 const loginUsuario = async (req, res) => {
   const { email, password, codigo_empresa } = req.body;
   try {
-    console.log('Tentativa de login com:', email); 
+    console.log('Tentativa de login com:', email);
 
-    const user = await pool.query('SELECT * FROM tb_cad_usuario WHERE email = $1 AND codigo_empresa = $2', [email, codigo_empresa]);
+    const user = await pool.query(
+      'SELECT * FROM tb_cad_usuario WHERE email = $1 AND codigo_empresa = $2',
+      [email, codigo_empresa]
+    );
 
     if (user.rows.length === 0) {
       return res.status(400).json({ message: 'Usuário não encontrado para a empresa selecionada' });
     }
 
-    console.log('Usuário encontrado:', user.rows[0]); 
+    console.log('Usuário encontrado:', user.rows[0]);
 
     const validPassword = await bcrypt.compare(password, user.rows[0].senha);
 
@@ -59,7 +72,10 @@ const loginUsuario = async (req, res) => {
 const obterUsuario = async (req, res) => {
   try {
     const userId = req.user.id;
-    const usuario = await pool.query('SELECT codigo, usuario, email, codigo_empresa FROM tb_cad_usuario WHERE codigo = $1', [userId]);
+    const usuario = await pool.query(
+      'SELECT codigo, usuario, email, codigo_empresa FROM tb_cad_usuario WHERE codigo = $1',
+      [userId]
+    );
     res.json(usuario.rows[0]);
   } catch (error) {
     res.status(500).send('Erro no servidor');

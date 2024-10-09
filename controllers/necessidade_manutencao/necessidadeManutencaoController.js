9b76772bf4899060cc0f06aa6bdb371abdfc8b72
Gabriel <Gabrielpedrofc@gmail.com>
2024-10-08 23:04:51 -0300

    func para atualizar NM

--- controllers/necessidade_manutencao/necessidadeManutencaoController.js
+++ controllers/necessidade_manutencao/necessidadeManutencaoController.js
@@ -52,6 +52,76 @@ const novaNM = async (req, res) => {
   }
 };
 
+//REVIEW - Testar
+const atualizarNM = async (req, res) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    const decoded = decodeJWT(token);
+
+    if (!decoded) {
+      return res.status(401).send('Token inválido ou expirado');
+    }
+
+    const { 
+            body_codigo_nm               ,
+            body_solicitante             ,
+            body_aprovador               ,
+            body_descricao               ,
+            body_observacao              ,
+            body_codigo_parceiro_negocio ,
+            body_nome_contato            ,
+            body_metodo_contato          ,
+            body_codigo_tpo_manutencao   ,
+            body_codigo_nivel_prioridade ,
+            body_desconto_bruto_geral    ,
+            body_acrescimo_bruto_geral   ,
+          } = req.body;
+    const jwt_codigo_empresa = decoded.codigo_empresa;
+    const jwt_codigo_usuario = decoded.id;
+    
+    await sequelize.query(`
+      CALL sp_necessidade_manutencao_update(
+        :p_codigo_nm                ::BIGINT,
+        :p_codigo_empresa           ::VARCHAR,
+        :p_solicitante              ::VARCHAR,
+        :p_aprovador                ::VARCHAR,
+        :p_descricao                ::TEXT,
+        :p_observacao               ::TEXT,
+        :p_codigo_parceiro_negocio  ::INTEGER,
+        :p_nome_contato             ::VARCHAR,
+        :p_metodo_contato           ::VARCHAR,
+        :p_codigo_usuario           ::INTEGER,
+        :p_codigo_tpo_manutencao    ::INTEGER,
+        :p_codigo_nivel_prioridade  ::INTEGER,
+        :p_desconto_bruto_geral     ::NUMERIC,
+        :p_acrescimo_bruto_geral    ::NUMERIC
+      )
+    `, {
+      replacements: {
+        p_codigo_nm                : body_codigo_nm               ,
+        p_codigo_empresa           : jwt_codigo_empresa           ,
+        p_solicitante              : body_solicitante             ,
+        p_aprovador                : body_aprovador               ,
+        p_descricao                : body_descricao               ,
+        p_observacao               : body_observacao              ,
+        p_codigo_parceiro_negocio  : body_codigo_parceiro_negocio ,
+        p_nome_contato             : body_nome_contato            ,
+        p_metodo_contato           : body_metodo_contato          ,
+        p_codigo_usuario           : jwt_codigo_usuario           ,
+        p_codigo_tpo_manutencao    : body_codigo_tpo_manutencao   ,
+        p_codigo_nivel_prioridade  : body_codigo_nivel_prioridade ,
+        p_desconto_bruto_geral     : body_desconto_bruto_geral    ,
+        p_acrescimo_bruto_geral    : body_acrescimo_bruto_geral   ,
+      }
+    });
+
+    res.status(201).send('NM atualizada com sucesso');
+  } catch (err) {
+    console.error('Erro ao atualizar a NM:', err.message);
+    res.status(500).send('Erro no servidor');
+  }
+};
+
 // Função para upload de foto de ativo
 const uploadFoto = async (req, res) => {
   try {
@@ -108,4 +178,5 @@ const uploadFoto = async (req, res) => {
 module.exports = {
   novaNM,
   uploadFoto,
+  atualizarNM,
 };
--- routes/necessidade_manutencao/necessidadeManutencaoRouter.js
+++ routes/necessidade_manutencao/necessidadeManutencaoRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { 
     novaNM,
     uploadFoto,
+    atualizarNM,
 } = require('../../controllers/necessidade_manutencao/necessidadeManutencaoController');
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 const upload = require('../../middlewares/midMulterManutencao'); 
 
 router.post('/necessidade_manutencao/criar', novaNM);
+router.put('/necessidade_manutencao/atualizar', atualizarNM);
 router.post('/ativos/:codigo_nm/foto', upload.single('file'), uploadFoto);
 
 module.exports = router;
